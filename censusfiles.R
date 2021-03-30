e1a15627de4ed1d7fadda14626b2196e8b6e7e71
andrewjbe <56839927+andrewjbe@users.noreply.github.com>
2021-03-30 11:24:08 -0500

    initial commit

---
+++ censusfiles.R
@@ -0,0 +1,25 @@
+library(dplyr)
+
+# setwd("F:/Andrew/Documents/Dropbox/R Projects/Fixing Census Data files/")
+# setwd("/Users/andrewbell/Dropbox (Personal)/R Projects/Fixing Census Data files/")
+
+# Read csv files
+ds.hh <- read_csv("hhpub19.csv")
+ds.pp <- read_csv("pppub19.csv")
+
+# Get rid of cols we don't need from home file, rename to id_match
+ds.hh <- ds.hh %>%
+  dplyr::select(H_IDNUM, GESTFIPS) %>%
+  rename(id_match = H_IDNUM) %>%
+  mutate(id_match = substr(id_match, 1, 20))
+
+# Get substring of person ID num
+ds.pp <- ds.pp %>%
+  mutate(
+    id_match = substr(PERIDNUM, 1, 20)
+  )
+
+ds.combined <- merge(ds.hh, ds.pp, by = "id_match")
+
+ds.combined <- ds.combined %>%
+  filter(GESTFIPS == 40)
